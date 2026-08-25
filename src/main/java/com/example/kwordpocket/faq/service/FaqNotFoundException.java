10dcbff6b66437b80c1f0384c93e69613c75bc4a
piepenie <piepenie@daum.net>
2026-08-25 17:24:31 +0900

    refactor: 중복 FaqNotFoundException 삭제 및 공통 예외 연동 준비

--- src/main/java/com/example/kwordpocket/faq/service/FaqNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.kwordpocket.faq.service;
-
-public class FaqNotFoundException extends RuntimeException {
-    public FaqNotFoundException(String message) {
-        super(message);
-    }
-}
